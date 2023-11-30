db6762f78932c56ca1e40867d7de56609c8d732b
KimKyeongSeog <kimks9241@naver.com>
2023-11-30 10:39:48 +0900

    intersection-observer

---
+++ src/components/A.jsx
@@ -0,0 +1,13 @@
+const A = () => {
+  return (
+    <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center">
+      <img
+        className="w-96 h-96  animate__animated animate__tada animate__infinite"
+        src="./images/spongebob.jpg"
+        alt="bob"
+      />
+    </div>
+  );
+};
+
+export default A;
